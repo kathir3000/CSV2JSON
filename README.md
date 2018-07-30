a5e1c31a4ea1f2e09298ede9df9a14e846340db5
T.Eezhakathir <kathir3000@gmail.com>
2018-07-31 00:09:21 +0530

    Echarts heatmap format json conversion added

--- README.md
+++ README.md
@@ -10,8 +10,17 @@ npm Install
 "csvFileLocation":"./src/input.csv"
 "jsonOutputFileLocation":"./src/output.json"
 
+#Command Line Arguments
+#output json format; default 'grid'
+-f or --format [ grid | heatmap ] 
+#input file location; default taken from config file
+-i or -c or --csvFileLocation <input csv file path>
+#output file location;  default taken from config file
+-o or -j or --jsonOutputFileLocation <output Json file path>
+
+
 #run NodeJs
-node index.js
+node index.js [options]
 
 #File will be saved to the output location given in config file
 -T.Eezhakathir
--- app/index.js
+++ app/index.js
@@ -1,10 +1,43 @@
 
 const config = require('./config.json');
 const fs = require('fs');
-const csvFilePath = config.csvFileLocation;
-
 const csv = require('csvtojson');
 var _ = require('lodash');
+const buildOptions = require('minimist-options');
+var minimist = require('minimist');
+
+
+// Command Line arguments options
+const options = buildOptions({
+    format: {
+        type: 'string',
+        alias: 'f',
+        default: 'grid'
+    },
+    data: {
+        type: 'string',
+        alias: 'd',
+        default: ''
+    },
+    csvFileLocation: {
+        type: 'string',
+        alias: ['c', 'i'],
+        default: config.csvFileLocation
+    },
+    jsonOutputFileLocation: {
+        type: 'string',
+        alias: ['j', 'o'],
+        default: config.jsonOutputFileLocation
+    },
+
+    // special option for positional arguments (`_` in minimist)
+    arguments: 'string'
+});
+
+const args = minimist(process.argv.slice(2), options);
+const csvFilePath = args.csvFileLocation;
+const jsonFilePath = args.jsonOutputFileLocation;
+
 
 csv().fromFile(csvFilePath)
     .then((jsonObj) => {
@@ -12,43 +45,75 @@ csv().fromFile(csvFilePath)
     })
 
 function jsonMapping(jsonFromCsv) {
-    var finalJsonData = {};
-    let jsonKeys = Object.keys(jsonFromCsv[0]);
-    let columnNames = {};
+    if (args.format.toLowerCase() == 'grid') {
+        var finalJsonData = {};
+        let jsonKeys = Object.keys(jsonFromCsv[0]);
+        let columnNames = {};
 
-    var finalGridColumn = jsonKeys.map((value, index) => {
-        columnNames[value] = config.colName + (index + 1);
-        var attrib = {
-            "defaultF": "Y",
-            "attributeName": value,
-            "width": 150,
-            "attributeOrder": 1,
-            "colName": config.colName + (index + 1),
-            "dbColumn": "BETA_PLAN_ID",
-            "dbColumnType": "Varchar"
-        }
-        return attrib;
-    });
-
-    var finalGridData = jsonFromCsv.map((data) => {
-        var data = _.mapKeys(data, (value, key) => {
-            return columnNames[key];
+        var finalGridColumn = jsonKeys.map((value, index) => {
+            columnNames[value] = config.colName + (index + 1);
+            var attrib = {
+                "defaultF": "Y",
+                "attributeName": value,
+                "width": 150,
+                "attributeOrder": 1,
+                "colName": config.colName + (index + 1),
+                "dbColumn": "BETA_PLAN_ID",
+                "dbColumnType": "Varchar"
+            }
+            return attrib;
         });
-        return data;
-    });
 
-    finalJsonData.colConf = {}
-    finalJsonData.colConf.gridId = 1;
-    finalJsonData.colConf.gridName = "";
-    finalJsonData.colConf.attributes = finalGridColumn
+        var finalGridData = jsonFromCsv.map((data) => {
+            var data = _.mapKeys(data, (value, key) => {
+                return columnNames[key];
+            });
+            return data;
+        });
 
-    finalJsonData.data = finalGridData;
-    fs.writeFile(config.jsonOutputFileLocation, JSON.stringify(finalJsonData, null, 2), 'utf8', function (err) {
-        if (err) {
-            return console.log(err);
-        } 
-        console.log("CSV File converted successfully & Saved at '"+ config.jsonOutputFileLocation + "'");
-    });
+        finalJsonData.colConf = {}
+        finalJsonData.colConf.gridId = 1;
+        finalJsonData.colConf.gridName = "";
+        finalJsonData.colConf.attributes = finalGridColumn
+
+        finalJsonData.data = finalGridData;
+    }
+    else if (args.format.toLowerCase() == 'heatmap') {
+        let xAxis = Object.keys(jsonFromCsv[0]).slice(1); //xAxis
+        let yAxis = jsonFromCsv.map(item => item.field1); //yAxis
+        let data = jsonFromCsv.map(item => { 
+            delete item.field1
+            return Object.values(item);
+        });
+        data = _.flatten(data);
+        let heatMapData = [];
+        index = 0;
+        for (i=0;i<yAxis.length;i++){
+            for(j=0;j<xAxis.length;j++){
+               heatMapData.push([i,j,data[index]]);
+               index++;
+            }
+        }
+        finalJsonData = {
+            xAxis,yAxis,heatMapData
+        }
+        console.log(finalJsonData);
+        
+
+    }
+
+    if (finalJsonData) {
+        fs.writeFile(jsonFilePath, JSON.stringify(finalJsonData, null, 2), 'utf8',
+            function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("CSV File converted successfully & Saved at '" + jsonFilePath + "'");
+            });
+    }
+    else {
+        console.log("No data to process. Please check the input data in command line/config file");
+    }
 
 
 }
